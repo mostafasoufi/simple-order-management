f2eacda20cb6da2b2a7d6ae9197cb00e0e3e6160
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-11-23 15:18:14 +0330

    Added a new unit test for testing database.

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    /**
+     * Test the product table has default value.
+     *
+     * @return void
+     */
+    public function testProductsTable()
+    {
+        $this->assertDatabaseHas('products', [
+            'name' => 'Coca Cola',
+            'price' => '1.80',
+        ]);
+    }
+
+    /**
+     * Test the product discount table has default value.
+     *
+     * @return void
+     */
+    public function testProductDiscountTable()
+    {
+        $this->assertDatabaseHas('product_discount', [
+            'product_id' => '1',
+            'quantity' => '3',
+            'percent' => '20',
+        ]);
+    }
+}
